32d1a5086ac3fdc55d1a96f8342e9405b52440fd
nicohsfu <marcnicohernandez@gmail.com>
2023-01-12 21:15:47 -0800

    add state and useEffect hook to App component dynamically render pokemon cards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Navbar from './components/Navbar';
+import Card from './components/Card';
 import './App.scss';
 import './index.scss';
 
 function App() {
+  const [pokemonCards, setPokemonCards] = useState([]);
+
+  useEffect(() => {
+    axios.get('/pokemon')
+      .then(result => {
+        setPokemonCards(result.data.map(pokemon => {
+          <Card 
+            frontImage={pokemon.frontImage}
+            backImage={pokemon.backImage}
+            name={pokemon.name}
+            type={pokemon.type}
+          />
+        }))
+      });
+  }, [pokemonCards]);
 
   return (
     <>
       <Navbar />
-      <h1>App component</h1>
+      
+      <div className='app'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+
     </>
   );
 }
