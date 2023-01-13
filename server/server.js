0a4c674fbdb5fa1a5d91e37ec777d8b1b6a13ba6
Sam Julian <samjulian6@gmail.com>
2023-01-13 09:09:20 +0530

    Establish connection with MongoDb Atlas

--- server/server.js
+++ server/server.js
@@ -1,12 +1,40 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import trainerRoutes from './routes/trainers.js';
+import pokemonRoutes from './routes/pokemon.js';
+
+dotenv.config();
+
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
-const trainersRouter = require('./routes/trainers');
-const pokemonRouter = require('./routes/pokemon');
+app.use('/trainers', trainerRoutes);
+app.use('/pokemon', pokemonRoutes);
 
-app.use('/trainers', trainersRouter);
-app.use('/pokemon', pokemonRouter);
+app.use(bodyParser.json({
+  limit: "30mb",
+  extended: true
+}));
+
+app.use(bodyParser.urlencoded({
+  limit: "30mb",
+  extended: true
+}));
+
+app.use(cors);
+
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@pokestackcluster.mihvrie.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
+  .catch(error => console.log(error.message));
 
 app.use(function (req, res, next) {
   next(createError(404));
@@ -22,9 +50,3 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`)
-});
-
-module.exports = app;
