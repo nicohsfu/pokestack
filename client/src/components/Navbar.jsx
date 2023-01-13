4eb2be7da73f7528476cb9ab19c7d684d4ed8e56
nicohsfu <marcnicohernandez@gmail.com>
2023-01-12 20:27:15 -0800

    build navigation bar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.scss';
 
 function Navbar() {
   return (
-    <>
-      <h1>POKESTACK</h1>
-    </>
-  )
+    <div className='navbar'>
+      {/* <h1>POKESTACK</h1> */}
+      <img
+        className='pokestack-logo'
+        src='https://i.imgur.com/YrlmBrz.png'
+        alt='pokestack logo'
+      />
+
+      <div className='nav-right-half'>
+        <a href=''>Add Pokemon</a>
+        <p>Logged in as: Ash</p>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
