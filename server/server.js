2961c3a518da7c728b6bfb2f7fb19dd9a246bcca
Sam Julian <samjulian6@gmail.com>
2023-02-06 15:31:18 -0800

    Remove redundant functionality and add proper connection to database in connection URL

--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import cors from 'cors';
 import dotenv from 'dotenv';
 
 import trainerRoutes from './routes/trainers.js';
@@ -12,41 +10,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
+
 app.use('/trainers', trainerRoutes);
 app.use('/pokemon', pokemonRoutes);
 
-app.use(bodyParser.json({
-  limit: "30mb",
-  extended: true
-}));
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@pokestackcluster.mihvrie.mongodb.net/pokestack?retryWrites=true&w=majority`;
 
-app.use(bodyParser.urlencoded({
-  limit: "30mb",
-  extended: true
-}));
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true}, () => {
+  console.log('Connection to database has been established');
+})
 
-app.use(cors);
-
-const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@pokestackcluster.mihvrie.mongodb.net/?retryWrites=true&w=majority`;
-
-mongoose.connect(CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
-  .catch(error => console.log(error.message));
-
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
