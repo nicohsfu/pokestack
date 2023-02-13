155bd99b8cd1b280c7ec9fdfee08c78238a923f3
nicohsfu <marcnicohernandez@gmail.com>
2023-02-13 15:40:31 -0800

    implement react router to navigate our various pages/components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Navbar from './components/Navbar';
-import Card from './components/Card';
-import './App.scss';
+import React from 'react';
+import Navbar from './components/Navbar';import './App.scss';
 import './index.scss';
+import { Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import Search from './components/Search';
+import Info from './components/Info';
 
 function App() {
-  const [pokemonCards, setPokemonCards] = useState([]);
-
-  const handleDelete = (id) => {
-    const filteredPokemon = pokemonCards.filter(pokemon => pokemon.id !== id);
-    setPokemonCards(filteredPokemon);
-    axios.delete(`/pokemon/${id}`)
-      .then(result => console.log("deleted item", result));
-  };
-
-  useEffect(() => {
-    axios.get('/pokemon')
-      .then(result => {
-        setPokemonCards(result.data.map((pokemon, index) => {
-          return (
-            <Card
-              key={index}
-              id={pokemon._id}
-              frontImage={pokemon.frontImage}
-              backImage={pokemon.backImage}
-              name={pokemon.name}
-              type={pokemon.type.length > 1 ?
-                `${pokemon.type[0]} | ${pokemon.type[1]}` :
-                `${pokemon.type[0]}`
-              }
-              handleDelete={handleDelete}
-            />
-          );
-        }));
-      });
-  }, [pokemonCards]);
 
   return (
     <div className='app'>
       <Navbar />
-
-      <h1>Your Team</h1>
-      <h2 className='subheader'>Roster: {pokemonCards.length} / 6</h2>
-
-      <div className='cards'>
-        {pokemonCards}
-      </div>
+        <Routes>
+          <Route
+            path='/'
+            element={<Home />}
+          />
+          <Route
+            path='/search'
+            element={<Search />}
+          />
+          <Route
+            path='/info'
+            element={<Info />}
+          />
+        </Routes>
     </div>
   );
 }
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import Card from './Card';
+import axios from 'axios';
+import './Home.scss';
+
+const Home = () => {
+  const [pokemonCards, setPokemonCards] = useState([]);
+
+  const viewInfo = (id) => {
+
+  };
+
+  const handleDelete = (id) => {
+    const filteredPokemon = pokemonCards.filter(pokemon => pokemon.id !== id);
+    setPokemonCards(filteredPokemon);
+    axios.delete(`/pokemon/${id}`)
+      .then(result => console.log("deleted item", result));
+  };
+
+  useEffect(() => {
+    axios.get('/pokemon')
+      .then(result => {
+        setPokemonCards(result.data.map((pokemon, index) => {
+          return (
+            <Card
+              key={index}
+              id={pokemon._id}
+              frontImage={pokemon.frontImage}
+              backImage={pokemon.backImage}
+              name={pokemon.name}
+              type={pokemon.type.length > 1 ?
+                `${pokemon.type[0]} | ${pokemon.type[1]}` :
+                `${pokemon.type[0]}`
+              }
+              handleDelete={handleDelete}
+            />
+          );
+        }));
+      });
+  }, [pokemonCards]);
+
+  return (
+    <div className='home'>
+      <h1>Your Team</h1>
+      <h2 className='subheader'>Roster: {pokemonCards.length} / 6</h2>
+      <div className='cards'>
+        {pokemonCards}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
