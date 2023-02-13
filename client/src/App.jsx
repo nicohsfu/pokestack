3247c5c3d0f19b5282ec0200cd892e82e3794088
nicohsfu <marcnicohernandez@gmail.com>
2023-02-13 14:21:14 -0800

    conditionally separate render of pokemon types

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,24 +18,22 @@ function App() {
               frontImage={pokemon.frontImage}
               backImage={pokemon.backImage}
               name={pokemon.name}
-              type={pokemon.type}
+              type={pokemon.type.length > 1 ?
+                `${pokemon.type[0]} | ${pokemon.type[1]}` :
+                `${pokemon.type[0]}`
+              }
             />
-          )
-        }))
+          );
+        }));
       });
   }, [pokemonCards]);
 
   return (
     <div className='app'>
       <Navbar />
-      
-      <div className='cards'>
-        {/* <Card name="Squirtle" type="Water" frontURL="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" rearURL="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/7.png"/>
-        <Card name="Ivysaur" type="Grass" frontURL="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png" rearURL="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/2.png"/>
-        <Card name="Charizard" type="Fire | Flying" frontURL="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png" rearURL="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/6.png"/> */
-        pokemonCards
-        }
 
+      <div className='cards'>
+        {pokemonCards}
       </div>
 
     </div>
