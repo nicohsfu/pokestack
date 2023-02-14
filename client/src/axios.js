aa3dfe3243229932e38519a87c14a978f43a8076
Sam Julian <samjulian6@gmail.com>
2023-02-14 00:34:59 -0800

    Increase number of pokemon results pulled from search

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const getPokemon = async () => {
-  const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
+  const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1279&offset=0');
+  console.log(response.data);
   return response.data;
 }
