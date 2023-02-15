5d1edcff5cbebbfc4b81d74eb61f70ba743a3ba9
nicohsfu <marcnicohernandez@gmail.com>
2023-02-15 10:46:46 -0800

    initial visual render of pokemon info page

--- client/src/components/Info.jsx
+++ client/src/components/Info.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from 'react';
+import './Info.scss';
+import { FaInfoCircle, FaTrash } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
-const Info = () => {
+function Info(props) {
   return (
-    <div>Info</div>
-  )
-}
+    // RENAME CSS CLASSES
+    <>
+      <div>
+        <button>
+          Back to Roster
+        </button>
+        <br /><br />
+      </div>
+      <div className='card'>
 
-export default Info
+        <div className='front-and-back-views'>
+          <div>
+            <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/392.png"} alt="front view" />
+          </div>
+          <div>
+            <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/392.png"} alt="back view" />
+          </div>
+        </div>
+
+        <div className='name-and-type-and-button'>
+
+          <div className='name-and-type'>
+            <h2>Infernape</h2>
+            <p>fire | fighting</p>
+            <p>Blaze</p>
+          </div>
+
+        </div>
+
+        <div>
+          <table>
+            <tr>
+              <td>HP</td>
+              <td>44</td>
+            </tr>
+            <tr>
+              <td>Attack</td>
+              <td>48</td>
+            </tr>
+            <tr>
+              <td>Defense</td>
+              <td>65</td>
+            </tr>
+            <tr>
+              <td>Special Attack</td>
+              <td>65</td>
+            </tr>
+            <tr>
+              <td>Special Defense</td>
+              <td>64</td>
+            </tr>
+            <tr>
+              <td>Speed</td>
+              <td>43</td>
+            </tr>
+            <tr>
+              <td>Total</td>
+              <td>314</td>
+            </tr>
+          </table>
+        </div>
+
+      </div>
+    </>
+
+  );
+};
+
+export default Info;
