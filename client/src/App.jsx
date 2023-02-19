8e90733913cb060a2cb13dc7fec1762b72b9b281
Sam Julian <samjulian6@gmail.com>
2023-02-19 15:12:06 -0800

    Add state to pass pokemon id from Home to Info components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Navbar from './components/Navbar';import './App.scss';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar'; import './App.scss';
 import './index.scss';
 import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
@@ -7,24 +7,27 @@ import Search from './components/Search';
 import Info from './components/Info';
 
 function App() {
+  const [selectedPokemon, setSelectedPokemon] = useState('');
 
   return (
     <div className='app'>
       <Navbar />
-        <Routes>
-          <Route
-            path='/'
-            element={<Home />}
-          />
-          <Route
-            path='/search'
-            element={<Search />}
-          />
-          <Route
-            path='/info'
-            element={<Info />}
-          />
-        </Routes>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home setSelectedPokemon={setSelectedPokemon} />}
+        />
+        <Route
+          path='/search'
+          element={<Search />}
+        />
+        <Route
+          path='/info'
+          element={<Info
+            id={selectedPokemon}
+          />}
+        />
+      </Routes>
     </div>
   );
 }
